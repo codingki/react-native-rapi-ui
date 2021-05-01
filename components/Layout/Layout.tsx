dba020247b6b21a853b6ee9cf1ac9bbf6bc8fd69
Kiki <codingki@gmail.com>
2021-05-02 04:26:48 +0700

    on progress darkmode

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -4,12 +4,13 @@ import {
 	SafeAreaView,
 	SafeAreaViewProps,
 } from 'react-native-safe-area-context';
-
+import { useTheme } from '../../provider/ThemeProvider';
 interface Props extends SafeAreaViewProps {
 	backgroundColor?: string;
 }
 
 const Layout: React.FC<Props> = (props: Props) => {
+	const { theme } = useTheme();
 	return (
 		<SafeAreaView
 			{...props}
@@ -18,7 +19,7 @@ const Layout: React.FC<Props> = (props: Props) => {
 				{
 					flex: 1,
 					backgroundColor:
-						props.backgroundColor || component.layout.backgroundColor,
+						props.backgroundColor || component[theme].layout.backgroundColor,
 				},
 			]}
 		>
--- examples/showcase/App.tsx
+++ examples/showcase/App.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
-import { useCachedResources } from 'react-native-rapi-ui';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Navigator from './src/navigation';
 import { enableScreens } from 'react-native-screens';
+import { ThemeProvider } from 'react-native-rapi-ui';
+
 export default function App() {
 	enableScreens();
 
-	const isLoadingComplete = useCachedResources();
-
-	if (!isLoadingComplete) {
-		return null;
-	} else {
-		return (
+	return (
+		<ThemeProvider>
 			<SafeAreaProvider>
 				<Navigator />
 			</SafeAreaProvider>
-		);
-	}
+		</ThemeProvider>
+	);
 }
